ffbcd81bcbcb85eab7797cc70fe0e0cc0cdfa8a1
Maydanik <tmaydanik@gmail.com>
2019-02-09 09:38:08 +0200

    start footer

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,35 @@ $(document).on('click', '.tabs_nav-link', function(e){
     $(tabId).show(400);
     $(tabId).siblings('.tabs_tab').hide(400);
   });
+
+  $(document).ready(function(){
+  $('.carousel').slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    slidesToShow: 4,
+    responsive:[
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+    });
+
+  
+
+  $('.js_login-popup').magnificPopup({
+    items: {
+      src: '#popup',
+      type: 'inline'
+    }
+  });
+
+  $(document).on('click', '.js_collapse', function(){
+    $(this).toggleClass('active');
+    $(this).next('.footer_col-main').slideToggle(400);
+    $(this).parents('.flex_col').siblings().find('.footer_col-main').slideUp(400);
+  });
+});
